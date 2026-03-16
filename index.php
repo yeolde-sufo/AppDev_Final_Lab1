14f268a4c3b62268ee174f9ab293fcec9a9122d6
Ahmed <ahmedsjahangeer@gmail.com>
2026-03-16 21:40:16 +0800

    changed the design a bit

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <?php 
 include 'header.php';
 ?>
-<h1> My Home Page </h1>
+
+<h1>>_ SYSTEM_ROOT<span class="cursor"></span></h1>
+
 <?php if (isset($_SESSION['email'])) { 
-echo "<h2>Welcome ".$_SESSION['name'].". Your email id is ".$_SESSION['email']."</h2>"; 
+    echo "<h2>Welcome user_".$_SESSION['name'].".</h2>";
+    echo "<p>Access granted for node: ".$_SESSION['email']."</p>"; 
 ?>
-<h3><a href="logout.php">Logout </a></h3>
-<?php } 
-else {
-	?>
-	<h3>Click <a href="login.php">here</a> to login</h3>
+    <br>
+    <h3><a href="logout.php">[ Execute Logout ]</a></h3>
+
+<?php } else { ?>
+    
+    <h2>Authentication Required</h2>
+    <br>
+    <p>
+        <a href="login.php" class="btn btn-primary">Login</a> 
+        &nbsp;&nbsp;&nbsp;
+        <a href="register.php" class="btn btn-success">Register</a>
+    </p>
+
 <?php } ?>
+
 </body>
-</html> 
+</html>
